a9495420112e10eded437bc540ddc6fcbf12e627
Branden Ogata <branden.s.ogata@gmail.com>
2015-03-09 09:37:33 -1000

    Comments out a line in the .gitignore file; this is not optimal and a more secure means of providing a secret key will be necessary in the future.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ics624TourismRoutes::Application.config.secret_key_base = '648db27c330520f85356652db27ae34738a558b9c39b8eeb6709849a987afb7494a4962db29ce9b2b62aa6e087f7c80b78c76868d59a0156f318da6dd561a47b'
